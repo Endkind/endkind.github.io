3a7655b6a1e8e1ee78e7f1edacb2caeddf40399c
Endkind <endkind.ender@gmail.com>
2026-05-09 22:58:33 +0200

    changed zu live fetch with fallback of data from last build

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <script setup lang="ts">
-  import { watchEffect } from "vue";
-  import Navbar from "@components/Navbar.vue";
-  import Repo from "@components/GitHub/Repo.vue";
-  import repos from "@json/github/repos.json";
-  import profile from "@json/github/profile.json";
+import {onMounted, ref, watchEffect} from "vue";
+import Navbar from "@components/Navbar.vue";
+import Repo from "@components/GitHub/Repo.vue";
+import {GitHubUtils} from "@ts/utils/github.ts";
+import profile from "@json/github/profile.json";
+import type {GitHubRepo} from "@ts/schemas/github.ts";
+import repos_json from "@json/github/repos.json";
 
-  watchEffect(() => {
-    document.title = profile.name || "GitHub Repos";
-  });
+const repos = ref<GitHubRepo[]>([]);
+
+watchEffect(() => {
+  document.title = profile.name || "GitHub Repos";
+});
+
+onMounted(async () => {
+  try {
+  repos.value = await GitHubUtils.get_repos_data(profile.name);
+  } catch {
+    repos.value = repos_json as GitHubRepo[];
+  }
+
+});
 </script>
 
 <template>
   <div class="mx-20">
     <nav class="mb-6">
-      <Navbar />
+      <Navbar/>
     </nav>
     <main>
       <h1 class="text-2xl font-bold mb-4">GitHub Repos ({{ repos.length }})</h1>
